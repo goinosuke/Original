a38ad76fce32ad05e87e750b036fb14ccd7649ca
GOINOSUKEPC\誠司 <taigo0424-13hiromi@ezweb.ne.jp>
2017-11-19 16:25:17 +0900

    first commit

---
+++ Release Game/Assets/wing_animation_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wing_animation_Script : MonoBehaviour {
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("wing1");
+        }
+	}
+}
