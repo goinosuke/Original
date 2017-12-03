398803b869d800490d5231ab4ab8311eec9f4c92
GOINOSUKEPC\誠司 <taigo0424-13hiromi@ezweb.ne.jp>
2017-12-03 16:12:05 +0900

    改良点　・キャラを前進させるスクリプト

---
+++ Release Game/Assets/jump.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+    float a = 5;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 6, 0);
+        }
+        /*
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, 0, -25);
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            transform.Rotate(0, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 0, 25);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.rotation = new Quaternion(0,0,0,0);
+        }
+        */
+    }
+}
