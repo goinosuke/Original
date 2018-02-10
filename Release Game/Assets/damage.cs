b9a98c980f4c9f2eadf6d1c57e4b14f375bb62c2
GOINOSUKEPC\誠司 <taigo0424-13hiromi@ezweb.ne.jp>
2018-02-11 03:30:05 +0900

    ちっちゃくなる奴と、回復、ステージの外側

--- Release Game/Assets/damage.cs
+++ Release Game/Assets/damage.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class damage : MonoBehaviour
 {
+	GameObject rightwing2;
+	GameObject leftwing2;
     [Header("体力")]
     public float HP = 2;
     // Use this for initialization
     void Start()
     {
-
+		rightwing2 = GameObject.FindGameObjectWithTag ("rightwing2");
+		leftwing2 = GameObject.FindGameObjectWithTag ("leftwing2");
     }
 
     // Update is called once per frame
@@ -22,11 +25,16 @@ public class damage : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag ("Enemy")) {
 			HP -= 1;
-
-		if (HP <= 0)
-		{
-			SceneManager.LoadScene("GameOver");
+			rightwing2.SetActive (false);
+			leftwing2.SetActive (false);
+			if (HP <= 0) {
+				SceneManager.LoadScene ("GameOver");
+			}
 		}
+	 if (other.gameObject.CompareTag ("recovery")) {
+			rightwing2.SetActive (true);
+			leftwing2.SetActive (true);
+			
 	}
 }
   
